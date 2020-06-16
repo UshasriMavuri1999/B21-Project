a1d65d6bdffb04e155663c2f6a0ffef5f0dd750e
Ushasri Mavuri <ushasrimavuri1999@gmail.com>
2020-06-16 16:52:25 +0530

    added routing

---
+++ sampleag/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public a = [' '];
+  email: string;
+  password: string;
+  displayName: string;
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+  signUp() {
+    // const email = this.email;
+    // const password = this.password;
+    // const displayName = this.displayName;
+    // this.router.navigate(['/trade']);
+    // this.handleResponse(this.displayName);
+    this.router.navigate(['/trade', String(this.a[0])]);
+  }
+
+  getName() {
+    return this.displayName;
+  }
+  fieldTextType: boolean;
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+
+  // handleResponse(data){
+  //   // this.Token.handle(data.access_token);
+  //   // this.Auth.changeAuthStatus(true);
+  //   sessionStorage.setItem('loggedUser', data.Username);
+  //   this.router.navigateByUrl('/trade');
+  // }
+}
